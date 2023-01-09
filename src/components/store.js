93776adb5ea8f603f2818a595c45eed0364a0cb3
nizhantiw <nizhantiw@gmail.com>
2023-01-09 21:10:10 +0530

    Update store.js

--- src/components/store.js
+++ src/components/store.js
@@ -1,6 +1,5 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-//creating slice with reducers
 const slice = createSlice({
   name: "hot-news",
   initialState: {
@@ -8,16 +7,46 @@ const slice = createSlice({
     articles: [],
   },
   reducers: {
-    setnews: (state, action) => {
-      state.articles = [...action.payload];
-      state.articlesNum = action.payload.length;
+    set(state, action) {
+      //   console.log("function Callled");
+
+      let temp = state.articles;
+      state.articles = temp.concat(action.payload.articles);
+      state.articlesNum = action.payload.num;
+
+      // state.articles = action.payload.articles;
+      // state.articlesNum = action.payload.articles.length;
+      //   console.log(data.articles, "array");
+      //   console.log("function executed");
+    },
+    setNum(state, action) {
+      state.articlesNum = action.payload.num;
     },
   },
 });
 
-//store configration
 const store = configureStore({
-  reducer: { news: slice.reducer },
+  reducer: {
+    hotNews: slice.reducer,
+  },
 });
 
+// export const getData = (num) => {
+//   return async function (dispatch) {
+//     fetch(
+//       "https://newsapi.org/v2/top-headlines?country=us&apiKey=1bece6d43f784fba8e45baca9e8040b1"
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         dispatch(
+//           slice.actions.setData({
+//             articles: data.articles.slice(0, num),
+//           })
+//         );
+//       });
+//   };
+// };
+
 export default store;
+
+export const actions = slice.actions;
