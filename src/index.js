066db6b4e15da7431db46d56b27d3b0428083f38
nizhantiw <nizhantiw@gmail.com>
2023-03-28 18:44:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
 import { Provider } from "react-redux";
-import store from "./components/store";
+import store from "./store";
+import App from "./components/App";
 
 ReactDOM.render(
   <Provider store={store}>
